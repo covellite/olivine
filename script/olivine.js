d4ccd356b23d885b3d3e4c5b61635303f276161d
covellite <covellite.jp@gmail.com>
2014-06-12 17:06:07 +0900

    140612 ベース修正1

--- script/olivine.js
+++ script/olivine.js
@@ -3,29 +3,60 @@
 
     $.fn.olivine = function (o) {
         var d = {
-            speed: 300,
+            speed: 100,
             interval: 2000,
             direction: 'left'
         },
             intervalID,
             elemItemWrap = $(this).find('.item-wrap'),
             elemItem = $(this).find('.item'),
+            pclone = elemItem.children().clone(true),
+            aclone = elemItem.children().clone(true),
             itemW = elemItem.find('li').width(),
             itemL = elemItem.find('li').length,
+            elemPrev = $(this).find('.prev'),
+            elemNext = $(this).find('.next'),
             current = 0,
             fo = $.extend({}, d, o);
 
+        function prev() {
+            $('.nav').hide();
+            current = current - 1;
+            if (current < 0) {
+                elemItem.animate({
+                    left: itemW * Math.abs(current) - (itemW * itemL)
+                },
+                    fo.speed, function () {
+                        $('.nav').show();
+                        if (current == -1) {
+                            elemItem.css('left', -(itemW * itemL * 2 - itemW));
+                            current = itemL - 1;
+                        }
+                    });
+            } else {
+                elemItem.animate({
+                    left: -(itemW * current) - (itemW * itemL)
+                },
+                    fo.speed, function () {
+                        $('.nav').show();
+                        if (current == -1) {
+                            elemItem.css('left', -(itemW * itemL * 2 - itemW));
+                            current = itemL - 1;
+                        }
+                    });
+            }
+        }
 
-        function slide(dir) {
+        function next() {
             $('.nav').hide();
             current = current + 1;
             elemItem.animate({
-                left: -(itemW * current)
+                left: -(itemW * current) - (itemW * itemL)
             },
                 fo.speed, function () {
                     $('.nav').show();
                     if (current == itemL) {
-                        elemItem.css('left', 0);
+                        elemItem.css('left', -(itemW * itemL));
                         current = 0;
                     }
                 });
@@ -33,18 +64,56 @@
 
         return this.each(function () {
 
-            elemItem.children().clone().appendTo(elemItem);
+            pclone.appendTo(elemItem);
+            aclone.prependTo(elemItem);
             elemItem.css('width', elemItem.find('li').length * itemW);
+            elemItem.css('left', -(itemW * itemL));
 
-            intervalID = setInterval(function () {slide(fo.direction); }, fo.interval);
+            if (fo.direction == 'left') {
+                intervalID = setInterval(function () {next(fo.direction); }, fo.interval);
 
-            elemItemWrap
-                .bind('mouseover', function () {
-                    clearInterval(intervalID);
-                })
-                .bind('mouseout', function () {
-                    intervalID = setInterval(function () {slide(fo.direction); }, fo.interval);
-                });
+                elemItemWrap
+                    .bind('mouseover', function () {
+                        clearInterval(intervalID);
+                    })
+                    .bind('mouseout', function () {
+                        intervalID = setInterval(function () {next(fo.direction); }, fo.interval);
+                    });
+            } else if (fo.direction == 'right') {
+                intervalID = setInterval(function () {prev(); }, fo.interval);
+
+                elemItemWrap
+                    .bind('mouseover', function () {
+                        clearInterval(intervalID);
+                    })
+                    .bind('mouseout', function () {
+                        intervalID = setInterval(function () {prev(); }, fo.interval);
+                    });
+            }
+
+            elemPrev.bind('click', function(e) {
+                prev();
+                clearInterval(intervalID);
+                if (fo.direction == 'left') {
+                    intervalID = setInterval(function () {next(); }, fo.interval);
+                } else if (fo.direction == 'right') {
+                    intervalID = setInterval(function () {prev(); }, fo.interval);
+
+                }
+                e.preventDefault();
+            });
+
+            elemNext.bind('click', function(e) {
+                next();
+                clearInterval(intervalID);
+                if (fo.direction == 'left') {
+                    intervalID = setInterval(function () {next(); }, fo.interval);
+                } else if (fo.direction == 'right') {
+                    intervalID = setInterval(function () {prev(); }, fo.interval);
+
+                }
+                e.preventDefault();
+            });
         });
     };
 
